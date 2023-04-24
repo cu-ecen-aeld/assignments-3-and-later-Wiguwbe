84e09d80c4265153ffc0d2b60b7e5d16c73c228e
Tiago Teixeira <tiago.t@sapo.pt>
2023-04-24 14:27:55 +0100

    assignment 7.1

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -29,9 +29,53 @@
 struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct aesd_circular_buffer *buffer,
             size_t char_offset, size_t *entry_offset_byte_rtn )
 {
-    /**
-    * TODO: implement per description
-    */
+    bool out_eq_in = buffer->out_offs == buffer->in_offs;
+    uint8_t read_offs = buffer->out_offs;
+    // make sure we don't over-iterate
+    // this also helps with full buffer
+    for(uint8_t i=0; i<AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;i++)
+    {
+        if(
+            read_offs == buffer->in_offs
+            &&
+            (
+                ( out_eq_in && !buffer->full)
+                ||
+                !out_eq_in
+            )
+        )
+        {
+            /*
+                NOTES REGARDING THIS CHECK
+
+                With the enclosing for loop, we guarantee that we will not
+                rollover to re-read already visited nodes, so,
+                if we catch up with `in_offs` it's either:
+
+                - `out_offs == in_offs`, which, because of the enclosing for loop
+                will only happen if it's in the beggining (will break before we reach it again)
+                and, if the buffer is not full, then it's empty
+
+                or
+
+                - `out_offs != in_offs`, which means we reached the end.
+            */
+            // means we didn't find it
+            break;
+        }
+        if(char_offset < buffer->entry[read_offs].size)
+        {
+            // cool, let's read it
+            *entry_offset_byte_rtn = char_offset;
+            return buffer->entry+read_offs;
+        }
+        // else, continue searching
+        char_offset -= buffer->entry[read_offs].size;
+        read_offs ++;
+        if(read_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+            read_offs = 0;
+    }
+    // (for) else, it wasn't found
     return NULL;
 }
 
@@ -44,9 +88,19 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
-    /**
-    * TODO: implement per description
-    */
+    // copy
+    memcpy(buffer->entry+buffer->in_offs, add_entry, sizeof(struct aesd_buffer_entry));
+    // increment
+    buffer->in_offs ++;
+    // rollover
+    if(buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
+        buffer->in_offs = 0;
+    if(buffer->full)
+        // also increment out_offs
+        buffer->out_offs = buffer->in_offs;
+    // check for catch-up (no need to check if we know it's already full)
+    else if(buffer->in_offs == buffer->out_offs)
+        buffer->full = true;
 }
 
 /**
